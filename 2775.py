071a92ade78f37b4f42347beff74667063897951
남기돈 <nkd0310@naver.com>
2021-02-08 10:26:41 +0900

    기본수학1_2775_수정

--- 2775.py
+++ 2775.py
@@ -14,13 +14,7 @@ T = int(input())
 
 apartment = [[0]*15 for _ in range(15)]
 
-for i in range(15):
-    apartment[0][i] = i
-for j in range(15):
-    apartment[j][1] = 1
-
 for _ in range(T):
     k = int(input())
     n = int(input())
-    apartment[k][n] = result(k,n)
-    print(apartment[k][n])
+    print(result(k,n))
