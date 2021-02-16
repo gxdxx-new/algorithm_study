bb05667530abc0739ef1d421abd72f87f0704649
남기돈 <nkd0310@naver.com>
2021-02-16 20:07:33 +0900

    정렬_2108

---
+++ 정렬_2108.py
@@ -0,0 +1,28 @@
+import sys
+from collections import Counter
+
+N = int(sys.stdin.readline())
+numList = []
+for _ in range(N):
+    numList.append(int(sys.stdin.readline()))
+numList.sort()
+
+avg = sum(numList) // len(numList)
+print(avg)
+
+if(N == 1):
+    median = numList[0]
+else:
+    median = numList[N // 2]
+print(median)
+
+if(N == 1):
+    print(numList[0])
+else:
+    c = Counter(numList).most_common(2)
+    if(c[0][1] == c[1][1]):
+        print(c[1][0])
+    else:
+        print(c[0][0])
+
+print(numList[-1] - numList[0])
