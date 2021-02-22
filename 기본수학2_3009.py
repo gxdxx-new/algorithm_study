308151e0e8d3f7c5f35ea566e0c5ecd69d139aca
nkd0310 <nkd0310@naver.com>
2021-02-22 22:25:16 +0900

    기본수학2_3009

---
+++ 기본수학2_3009.py
@@ -0,0 +1,12 @@
+xList = []
+yList = []
+for _ in range(3):
+    x, y = map(int, input().split())
+    xList.append(x)
+    yList.append(y)
+for i in range(3):
+    if(xList.count(xList[i]) == 1):
+        x = xList[i]
+    if(yList.count(yList[i]) == 1):
+        y = yList[i]
+print(x, y)
