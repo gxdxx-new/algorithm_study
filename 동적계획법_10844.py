b422b9a5f8f21500f1274eeb4390c003ec29ca79
gxdxx <nkd0310@naver.com>
2021-07-06 13:00:59 +0900

    refactor: 반복문 코드 정리

--- 동적계획법_10844.py
+++ 동적계획법_10844.py
@@ -6,19 +6,13 @@ for i in range(1, 10):
     dp[1][i] = 1
 
 for i in range(2, N + 1):
-    dp[i][0] = dp[i-1][1]
-    dp[i][1] = dp[i-1][0] + dp[i-1][2]
-    dp[i][2] = dp[i-1][1] + dp[i-1][3]
-    dp[i][3] = dp[i-1][2] + dp[i-1][4]
-    dp[i][4] = dp[i-1][3] + dp[i-1][5]
-    dp[i][5] = dp[i-1][4] + dp[i-1][6]
-    dp[i][6] = dp[i-1][5] + dp[i-1][7]
-    dp[i][7] = dp[i-1][6] + dp[i-1][8]
-    dp[i][8] = dp[i-1][7] + dp[i-1][9]
-    dp[i][9] = dp[i-1][8]
+    for j in range(10):
+        if(j == 0):
+            dp[i][j] = dp[i-1][1]
+        elif(j == 9):
+            dp[i][j] = dp[i - 1][j - 1]
+        else:
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
 
-count = 0
-for i in range(10):
-    count += dp[-1][i]
+print(sum(dp[-1]) % 1000000000)
 
-print(count % 1000000000)
