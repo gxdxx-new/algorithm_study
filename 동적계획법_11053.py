ebabb51fe845aafebd222ccaf3851402fd1350b2
gxdxx <nkd0310@naver.com>
2021-07-08 13:14:48 +0900

    동적계획법_11053

---
+++ 동적계획법_11053.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+dp = [0] * N
+
+A = list(map(int, input().split()))
+
+for i in range(N):
+    for j in range(i):
+        if(A[i] > A[j] and dp[i] < dp[j]):
+            dp[i] = dp[j]
+    dp[i] += 1
+
+print(max(dp))
